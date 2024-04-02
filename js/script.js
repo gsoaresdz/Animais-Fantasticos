5f26afdf236dfc9ae045a6eb2c367396d161bd73
Gabriel Soares <69989654+gsoaresdz@users.noreply.github.com>
2024-04-02 15:51:15 +0000

    menu mobile refatorado

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
+import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
+import TabNav from './modules/tabnav.js';
+import Modal from './modules/modal.js';
+import Tooltip from './modules/tooltip.js';
 import DropdownMenu from './modules/dropdown-menu.js';
+import MenuMobile from './modules/menu-mobile.js';
+import initFuncionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
-import initFuncionamento from './modules/funcionamento.js';
-import initMenuMobile from './modules/menu-mobile.js';
-import Modal from './modules/modal.js';
 import ScrollAnima from './modules/scroll-anima.js';
-import ScrollSuave from './modules/scroll-suave.js';
-import TabNav from './modules/tabnav.js';
-import Tooltip from './modules/tooltip.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -16,17 +16,10 @@ scrollSuave.init();
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 
-const tabNav = new TabNav(
-  '[data-tab="menu"] li',
-  '[data-tab="content"] section',
-);
+const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tabNav.init();
 
-const modal = new Modal(
-  '[data-modal="abrir"]',
-  '[data-modal="fechar"]',
-  '[data-modal="container"]',
-);
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init();
 
 const tooltip = new Tooltip('[data-tooltip]');
@@ -38,7 +31,9 @@ scrollAnima.init();
 const dropdownMenu = new DropdownMenu('[data-dropdown]');
 dropdownMenu.init();
 
-initMenuMobile();
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
+
 initFuncionamento();
 
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
