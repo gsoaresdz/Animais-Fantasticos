1eeed7c0c8e24e04155b3ddb0a284ecb1f2f34fe
Gabriel Soares <69989654+gsoaresdz@users.noreply.github.com>
2024-04-30 11:12:52 +0000

    adicionado slide de fotos

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -9,17 +9,17 @@ export default class Accordion {
     item.nextElementSibling.classList.toggle(this.activeClass);
   }
 
-  // Adicionar os aventos aos accordion
+  // adiciona os eventos ao accordion
   addAccordionEvent() {
     this.accordionList.forEach((item) => {
       item.addEventListener('click', () => this.toggleAccordion(item));
     });
   }
 
-  // Iniciar funçao
+  // iniciar função
   init() {
     if (this.accordionList.length) {
-      // Ativar primeiro item
+      // ativar primeiro item
       this.toggleAccordion(this.accordionList[0]);
       this.addAccordionEvent();
     }
--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,7 +1,8 @@
 import AnimaNumeros from './anima-numeros.js';
 
 export default function fetchAnimais(url, target) {
-  // Cria a div contendo informações com o total de animas
+  // Cria a div contendo informações
+  // com o total de animais
   function createAnimal(animal) {
     const div = document.createElement('div');
     div.classList.add('numero-animal');
@@ -11,7 +12,7 @@ export default function fetchAnimais(url, target) {
 
   // Preenche cada animal no DOM
   const numerosGrid = document.querySelector(target);
-  function preencheraAnimais(animal) {
+  function preencherAnimais(animal) {
     const divAnimal = createAnimal(animal);
     numerosGrid.appendChild(divAnimal);
   }
@@ -22,17 +23,17 @@ export default function fetchAnimais(url, target) {
     animaNumeros.init();
   }
 
-  // Puxa os animas através de um arquivo json
+  // Puxa os animais através de um arquivo json
   // e cria cada animal utilizando createAnimal
   async function criarAnimais() {
     try {
-      // Fetch, espera resposta e transforma em json
+      // Fetch, espera a resposta e transforma em json
       const animaisResponse = await fetch(url);
       const animaisJSON = await animaisResponse.json();
 
       // Após a transformação de json, ativa as funções
       // para preencher e animar os números
-      animaisJSON.forEach(animal => preencheraAnimais(animal));
+      animaisJSON.forEach(animal => preencherAnimais(animal));
       animaAnimaisNumeros();
     } catch (erro) {
       console.log(erro);
